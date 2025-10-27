cf6ea2d5b627f74f14aaa7d6545519ec99a52309
jayanth0075 <jayanth9b.vhs@gmail.com>
2025-10-27 09:27:25 +0530

    feat: add ContactMessage model for storing contact form submissions

---
+++ community/contact_models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.utils import timezone
+
+class ContactMessage(models.Model):
+    """Model for storing contact form submissions"""
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    subject = models.CharField(max_length=300)
+    message = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+    is_read = models.BooleanField(default=False)
+    replied_at = models.DateTimeField(null=True, blank=True)
+    
+    class Meta:
+        ordering = ['-created_at']
+        verbose_name = 'Contact Message'
+        verbose_name_plural = 'Contact Messages'
+    
+    def __str__(self):
+        return f"{self.name} - {self.subject}"
+    
+    def mark_as_read(self):
+        self.is_read = True
+        self.save()
